fd49622871763693c68090eb23cf5bb4130632b3
Joshua Simacek <joshua.simacek@icfi.com>
2017-03-23 02:09:47 -0400

    Authorization
    -Added UserName to Registration
    -Changed Email to UserName for Login
    -Removed ability to register for new users

--- myBills.web/Controllers/AccountController.cs
+++ myBills.web/Controllers/AccountController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -75,7 +72,7 @@ namespace myBills.web.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -134,6 +131,7 @@ namespace myBills.web.Controllers
             }
         }
 
+        /*
         //
         // GET: /Account/Register
         [AllowAnonymous]
@@ -151,7 +149,7 @@ namespace myBills.web.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
@@ -170,7 +168,7 @@ namespace myBills.web.Controllers
 
             // If we got this far, something failed, redisplay form
             return View(model);
-        }
+        }*/
 
         //
         // GET: /Account/ConfirmEmail
